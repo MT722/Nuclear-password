6e74c4fd29773e9dbbf6a50c7e78771b997a111f
MT722 <matthewto722@gmail.com>
2021-06-24 19:28:07 -0700

    made character variables for password, as well as window prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,10 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var alaphabet = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "123456789"
+var special =  escape("!\"#$%&\"'()*+,-/:;<=>?@[\]^_`{|}~");
+var generateBtn = document.querySelector("#generate");//calls the button created in html
+var characters = prompt("enter your desired charaacters(s for special, l for lower, u for upper, and n for number");//prompts user to enter password characters
+var length = prompt("enter your password length");
 
 // Write password to the #password input
 function writePassword() {
@@ -9,6 +14,7 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
